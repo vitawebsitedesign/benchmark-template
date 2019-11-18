215189550a52285e962ed4ed4466b088f7db0618
Michael Nguyen <mn@3dsafety.com.au>
2019-11-19 10:55:51 +1100

    initial commit

---
+++ Benchmark/Benchmark/Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private readonly IEnumerable<int> _enumerable;
+        private readonly IList<int> _list;
+
+        public Benchmark()
+        {
+            _enumerable = Enumerable.Range(0, 100);
+            _list = new List<int>(_enumerable);
+        }
+
+        // Set your own benchmarks below
+        [Benchmark]
+        public int ElementAtEnumerable() => _enumerable.ElementAt(99);
+        [Benchmark]
+        public int ElementAtList() => _list.ElementAt(99);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# benchmark-template
-.NET Core project template for benchmarking
+# What
+Project template for .NET Core benchmarking.
+
+This was made because setting these kinda projects takes time, and i just like to jump to the benchmarking action.
+
+https://github.com/dotnet/BenchmarkDotNet
+
+# Install
+```console
+git clone git@github.com:vitawebsitedesign/benchmark-template.git
+```
+
+# Usage
+Open solution in VS2019 & click "Debug > Start Without Debugging" button:
+
+This project template already comes with 2 example benchmarks.
+
+# Contributing
+Feel free to open a PR.
+
+The docs are also useful: [https://benchmarkdotnet.org/articles/guides/getting-started.html](https://benchmarkdotnet.org/articles/guides/getting-started.html)
